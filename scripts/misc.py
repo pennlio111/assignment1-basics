77d900531ea36b391c11045034b05d4e5843546c
Pennlio <pennlio111@users.noreply.github.com>
2025-09-14 17:15:43 -0400

    update

--- scripts/misc.py
+++ scripts/misc.py
@@ -16,9 +16,3 @@ with open("./data/my_bpe_merges.pkl", "wb") as f:
 
 space_index = [k for k, v in vocab.items() if v == b' ']
 print("Space token index:", space_index)  # should be [len(special_tokens) + 32]
-
-# longest_token = b""
-# for index, token in vocab.items():
-#     if len(token) > len(longest_token):
-#         longest_token = token
-# print(f"Longest token: {longest_token.decode('utf-8')} with length {len(longest_token)}") 
